ce24332768b3ddbeab05bc63c3159e818e1825fd
Ihor Koval <nicholakoval@gmail.com>
2021-04-09 18:15:02 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { ProgramComponent } from './components/program/program.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { StartComponent } from './components/start/start.component';
+import { AdvantagesComponent } from './components/advantages/advantages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainScreenComponent,
+    AboutComponent,
+    FooterComponent,
+    TeachersComponent,
+    ProgramComponent,
+    PartnersComponent,
+    StartComponent,
+    AdvantagesComponent
   ],
   imports: [
     BrowserModule,
